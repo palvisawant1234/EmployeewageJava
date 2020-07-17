b17203975c9325f42fb5cdd1e6c429aae9a4d185
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-18 03:10:01 +0530

    Added:fuctionailty to check daily employee wage

--- Bridgelabz/src/Employeewage.java
+++ Bridgelabz/src/Employeewage.java
@@ -5,10 +5,21 @@ public class Employeewage
 	{
 		System.out.println("Welcome to Employee Wage Computation");
 		int Attendance=1;
+		int EMP_WAGE_PER_HR=20;
+		int empHrs=0;
+		int empWage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==Attendance)
+		{
 			System.out.println("Employee is Present.");
+			empHrs=8;
+		}
 		else
+		{
 			System.out.println("Employee is Absent.");
+			empHrs=0;
+		}
+		empWage=empHrs*EMP_WAGE_PER_HR;
+		System.out.println("Daily Employee Wage:"+empWage);
 	}
 }
