3f235dd603bd62a4cb4e8002abc03aa844b85b57
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-18 03:24:58 +0530

    Added:functionality to calculate full time and part time wages

--- Bridgelabz/src/Employeewage.java
+++ Bridgelabz/src/Employeewage.java
@@ -4,21 +4,18 @@ public class Employeewage
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Employee Wage Computation");
-		int Attendance=1;
+		int PART_TIME=1;
+		int FULL_TIME=2;
 		int EMP_WAGE_PER_HR=20;
 		int empHrs=0;
 		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck==Attendance)
-		{
-			System.out.println("Employee is Present.");
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if(empCheck==FULL_TIME)
 			empHrs=8;
-		}
+		else if(empCheck==PART_TIME)
+			empHrs=4;
 		else
-		{
-			System.out.println("Employee is Absent.");
 			empHrs=0;
-		}
 		empWage=empHrs*EMP_WAGE_PER_HR;
 		System.out.println("Daily Employee Wage:"+empWage);
 	}
