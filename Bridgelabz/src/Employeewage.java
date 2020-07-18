7167affe9fbdb2ddf569b0867d74c583615e1fbf
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-18 14:17:58 +0530

    Added:fuctionality to calculate wages for month

--- Bridgelabz/src/Employeewage.java
+++ Bridgelabz/src/Employeewage.java
@@ -4,22 +4,29 @@ public class Employeewage
 		public static void main(String[] args)
 		{
 			int EMP_WAGE_PER_HR=20;
+			int NUM_OF_WORKING_DAYS=20;
 			int empHrs=0;
 			int empWage=0;
+			int totalEmpWage=0;
 			System.out.println("Welcome to Employee Wage Computation");
-			int empCheck=(int) Math.floor(Math.random()*10)%3;
-			switch(empCheck)
+			for(int day=1;day<=NUM_OF_WORKING_DAYS;day++)
 			{
-			case 1:
-				empHrs=4;
-				break;
-			case 2:
-				empHrs=8;
-				break;
-			default:
-				empHrs=0;
-			}
+				int empCheck=(int) Math.floor(Math.random()*10)%3;
+				switch(empCheck)
+				{
+					case 1:
+							empHrs=4;
+							break;
+					case 2:
+							empHrs=8;
+							break;
+					default:
+							empHrs=0;
+				}
 			empWage=empHrs*EMP_WAGE_PER_HR;
-			System.out.println("Todays Employee Wage:"+empWage);
+			totalEmpWage+=empWage;
+			System.out.println("Day "+day+" Employee Wage:"+empWage);
+			}
+			System.out.println("Total Employee Wage:"+totalEmpWage);
 		}
 }
