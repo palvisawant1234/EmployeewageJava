068720e2ac3a69e2469b891f5a5bb6516d335777
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-19 21:40:34 +0530

    Refactor:Computing employee wage using class methods

--- Bridgelabz/src/Employeewage.java
+++ Bridgelabz/src/Employeewage.java
@@ -1,36 +1,46 @@
 
 public class Employeewage 
 {
-		public static void main(String[] args)
+	public static final int PART_TIME=1;
+	public static final int FULL_TIME=2;
+	public static final int EMP_WAGE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int MAX_HRS_IN_MONTH=100;
+    
+    public static int computeEmpWage()
+	{
+		int empHrs=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
+		int totalEmpWage=0;
+		
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
-			int EMP_WAGE_PER_HR=20;
-			int NUM_OF_WORKING_DAYS=20;
-			int MAX_HRS_IN_MONTH=100;
-			int empHrs=0;
-			int totalEmpWage=0;
-			int totalEmpHrs=0;
-			int totalWorkingDays=0;
-			while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+			int empWage=0;
+			totalWorkingDays++;
+			int empCheck=(int) Math.floor(Math.random()*10)%3;
+			switch(empCheck)
 			{
-				int empWage=0;
-				totalWorkingDays++;
-				int empCheck=(int) Math.floor(Math.random()*10)%3;
-				switch(empCheck)
-				{
-					case 1:
-						empHrs=4;
-						break;
-					case 2:
-						empHrs=8;
-						break;
-					default:
-						empHrs=0;
-				}
-				totalEmpHrs+=empHrs;
-				empWage=empHrs*EMP_WAGE_PER_HR;
-				System.out.println("Day:"+totalWorkingDays+"  EmployeeHours:"+empHrs+"  Employee wage: "+empWage);
+				case PART_TIME:
+					empHrs=4;
+					break;
+				case(FULL_TIME):
+					empHrs=8;
+					break;
+				default:
+					empHrs=0;
 			}
-			totalEmpWage=totalEmpHrs*EMP_WAGE_PER_HR;
-			System.out.println("Total Employee Wage:"+totalEmpWage);
+			totalEmpHrs+=empHrs;
+			empWage=empHrs*EMP_WAGE_PER_HR;
+			System.out.println("Day:"+totalWorkingDays+"  Employee Hours:"+empHrs+"  Employee wage: "+empWage);
 		}
+		totalEmpWage=totalEmpHrs*EMP_WAGE_PER_HR;
+		System.out.println();
+		System.out.println("Total Employee Hours:"+totalEmpHrs+"	 Total Employee Wage:"+totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args)
+	{
+		computeEmpWage();
+	}
 }
