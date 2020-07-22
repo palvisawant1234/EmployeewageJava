abc0c040b9bc55052e2de0dc0132f182b23cbe8a
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-22 14:55:28 +0530

    Added:save dailywage along with total wage

--- Bridgelabz/src/Employeewage.java
+++ Bridgelabz/src/Employeewage.java
@@ -1,72 +1,104 @@
 import java.util.*;
-class Companyemployeewage 
+class CompanyemployeeWage 
 {
-	public static final int PART_TIME=1;
-	public static final int FULL_TIME=2;
-	
+	private static final int PART_TIME = 1;
+	private static final int FULL_TIME = 2;
+	private static final int fullDayHour = 8;
+
 	private static String company;
-	private static int fullDayHour;
-	private static int EMP_WAGE_PER_HR;
+	private static int EMP_RATE_PER_HR;
 	private static int MAX_HRS_IN_MONTH;
 	private static int NUM_OF_WORKING_DAYS;
-	
-	Companyemployeewage(String company, int EMP_WAGE_PER_HR, int fullDayHour, int MAX_HRS_IN_MONTH, int NUM_OF_WORKING_DAYS)
+	private static int empWage;
+	private static int entry;
+
+	public static HashMap<Integer, Integer> companyWage = new HashMap<Integer, Integer>();
+	public static ArrayList<Integer> Company1 = new ArrayList <Integer>();
+	public static ArrayList<Integer> Company2 = new ArrayList <Integer>();
+		
+	CompanyemployeeWage(String company, int EMP_RATE_PER_HR, int MAX_HRS_IN_MONTH, int NUM_OF_WORKING_DAYS) 
 	{
 		this.company=company;
-		this.EMP_WAGE_PER_HR = EMP_WAGE_PER_HR;
-		this.fullDayHour = fullDayHour;
+		this.EMP_RATE_PER_HR = EMP_RATE_PER_HR;
 		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
 		this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
 	}
-    
+
 	public String getCompany() 
 	{
 		return this.company;
 	}
-	
-	public static int computeEmpWage()
+
+	public static int employeeDailyWage() 
 	{
-		int empHrs=0;
-		int totalEmpHrs=0;
-		int totalWorkingDays=0;
-		int totalEmpWage=0;
-		
-		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+		empWage = fullDayHour * EMP_RATE_PER_HR;
+		return empWage;
+	}
+
+	public static int employeeMonthlyWage() 
+	{
+		int empWage = 0;
+		int empHrs = 0, totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		int fullDayHour = 8;
+		while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS )
 		{
-			int empWage=0;
-			totalWorkingDays++;
-			int empCheck=(int) Math.floor(Math.random()*10)%3;
-			switch(empCheck)
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) 
 			{
-				case PART_TIME:
-					empHrs=fullDayHour/2;
-					break;
 				case FULL_TIME:
-					empHrs=fullDayHour;
+					empHrs = fullDayHour;
+					break;
+				case PART_TIME:
+					empHrs = fullDayHour / 2;
 					break;
 				default:
-					empHrs=0;
+					empHrs = 0;
+					break;
 			}
-			totalEmpHrs+=empHrs;
-			empWage=empHrs*EMP_WAGE_PER_HR;
-			
+			totalEmpHrs += empHrs;
+			totalWorkingDays++;
+			empWage = EMP_RATE_PER_HR * empHrs;
+			if(company=="DMart")
+				Company1.add(empWage);
+			else
+				Company2.add(empWage);
+			totalEmpWage += empWage;
 		}
-		totalEmpWage=totalEmpHrs*EMP_WAGE_PER_HR;
-    		return totalEmpWage;
-    	}
+		return totalEmpWage;
+	}
+
+	public static void dailyWageAndMonthlyWage() 
+	{
+		companyWage.put(employeeDailyWage(), employeeMonthlyWage());
+		entry++;
+	}
+
+	public static void printWage() 
+	{
+		for (int i: companyWage.keySet())
+			System.out.println("Daily Wage:"+i+" Total Wage:"+companyWage.get(i));
+	}
+
+	public static void printCompanyWage() 
+	{
+		System.out.println(Company1);
+		System.out.println(Company2);
+	}
 }
 
 public class Employeewage 
 {
-	public static ArrayList <Integer> company = new ArrayList <Integer>();
+	
 	public static void main(String args[])
 	{
-		Companyemployeewage company1 = new Companyemployeewage("DMart", 20, 6, 110, 26);
-		Companyemployeewage company2 = new Companyemployeewage("Reliance", 24, 8, 135, 20);
-		Companyemployeewage company3 = new Companyemployeewage("Walmart", 20, 10, 120, 26);
-		company.add(company1.computeEmpWage());
-		company.add(company2.computeEmpWage());
-		company.add(company3.computeEmpWage());
-		System.out.println(company);
+		
+		CompanyemployeeWage company1 = new CompanyemployeeWage("DMart", 20, 110, 26);
+		company1.dailyWageAndMonthlyWage();
+		CompanyemployeeWage company2 = new CompanyemployeeWage("Reliance", 24, 135, 20);
+		company2.dailyWageAndMonthlyWage();
+		company2.printWage();
+		company2.printCompanyWage();
 	}
 }
